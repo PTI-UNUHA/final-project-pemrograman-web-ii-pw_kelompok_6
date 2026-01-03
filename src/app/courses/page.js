d7dfab6bedafd7d149e5eb11ea44876e0471b3ff
maya <mayaapriliaa14@gmail.com>
2026-01-03 10:08:22 +0700

    https://github.com/PTI-UNUHA/final-project-pemrograman-web-ii-pw_kelompok_6.git

---
+++ src/app/courses/page.js
@@ -0,0 +1,13 @@
+export default function Courses() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Daftar Kursus</h1>
+
+      <div>
+        <p>Web Programming</p>
+        <p>UI/UX Design</p>
+        <p>Backend Development</p>
+      </div>
+    </div>
+  );
+}
